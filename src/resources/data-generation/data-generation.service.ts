b906d85f63638bef4510741885d24a23d38e47c4
Akhil E M <akhilemcalicut@gmail.com>
2023-03-31 17:25:39 +0530

    edited data generation method

--- src/resources/data-generation/data-generation.service.ts
+++ src/resources/data-generation/data-generation.service.ts
@@ -5,7 +5,6 @@ import { CronJobService } from 'src/common/helpers/cron-job.service';
 import responseModel from 'src/common/models/api.model';
 import { MotorDataConfiguration } from 'src/database/entities/motor-data-configuration.entity';
 import { MotorType } from 'src/database/entities/motor-type.entity';
-import { literal } from 'sequelize';
 import { MotorData } from 'src/database/entities/motor-data.entity';
 @Injectable()
 export class DataGenerationService {
@@ -19,203 +18,184 @@ export class DataGenerationService {
       });
       if (!motorType)
         throw new HttpException('motor type not found', HttpStatus.NOT_FOUND);
-      const dataConfigurations = await MotorDataConfiguration.findOne({
+
+      let dataConfigurations = await MotorDataConfiguration.findOne({
         where: {
           motor_type_id: motorId,
         },
-        include: {
-          model: MotorType,
-        },
-        raw: true,
       });
 
       if (!dataConfigurations)
         throw new HttpException(
-          'not data configurations added.',
+          'motor configurations not found',
           HttpStatus.NOT_FOUND,
         );
 
-      if (action === 'start') {
-        //update data configuration
-        await MotorDataConfiguration.update(
-          { motor_data_generation_started: true },
-          {
-            where: {
-              motor_type_id: motorId,
+      //remove every previous data
+      await MotorData.destroy({
+        where: {
+          motor_type_id: parseInt(motorId),
+        },
+      });
+
+      const updateData = async () => {
+        const updateDataConfiguration = async (started: boolean) => {
+          await MotorDataConfiguration.update(
+            {
+              motor_data_generation_started: started,
             },
-          },
-        );
-        //start cron job
-        const minutes =
-          dataConfigurations['motor_data_configuration_interval_minutes'];
-        const seconds =
-          dataConfigurations['motor_data_configuration_interval_seconds'];
-        const interval =
-          minutes === 0 ? seconds * 1000 : 60 * minutes * 1000 + seconds * 1000;
-
-        //remove every previous data
-        await MotorData.destroy({
-          where: {
-            motor_type_id: parseInt(motorId),
-          },
-        });
-
-        const updateData = async () => {
-          try {
-            //setting starting action
-            let updateFunction: (val1: number, val2: number) => number;
-
-            updateFunction =
-              dataConfigurations['motor_data_configuration_starting_action'] ===
-              'increment'
-                ? (val1: number, val2: number) => val1 + val2
-                : (val1: number, val2: number) => val1 - val2;
-
-            //get old data count
-            const dataCount = await MotorData.count({
+            {
               where: {
-                motor_type_id: parseInt(motorId),
+                motor_type_id: motorId,
               },
-            });
+            },
+          );
+        };
+        //get configurations again if there is an update in configuration while
+        //generating data
+        dataConfigurations = await MotorDataConfiguration.findOne({
+          where: {
+            motor_type_id: motorId,
+          },
+          include: {
+            model: MotorType,
+          },
+          raw: true,
+        });
+        if (action == 'start') {
+          await updateDataConfiguration(true);
+          //start cron job
+          const minutes =
+            dataConfigurations['motor_data_configuration_interval_minutes'];
+          const seconds =
+            dataConfigurations['motor_data_configuration_interval_seconds'];
+          const interval =
+            minutes === 0
+              ? seconds * 1000
+              : 60 * minutes * 1000 + seconds * 1000;
 
-            // switch after intervals added or not
+          //setting starting action
+          let updateFunction: (val1: number, val2: number) => number;
+
+          updateFunction =
+            dataConfigurations['motor_data_configuration_starting_action'] ===
+            'increment'
+              ? (val1: number, val2: number) => val1 + val2
+              : (val1: number, val2: number) => val1 - val2;
+
+          //get old data count
+          const dataCount = await MotorData.count({
+            where: {
+              motor_type_id: parseInt(motorId),
+            },
+          });
+
+          // switch after intervals added or not
+          if (
+            dataConfigurations[
+              'motor_data_configuration_switch_after_occurrences'
+            ] > 0
+          ) {
+            //switch action
             if (
+              dataCount >=
               dataConfigurations[
                 'motor_data_configuration_switch_after_occurrences'
-              ] > 0
+              ] -
+                1
             ) {
-              //switch action
-              if (
-                dataCount >=
+              updateFunction =
                 dataConfigurations[
-                  'motor_data_configuration_switch_after_occurrences'
-                ] -
-                  1
-              ) {
-                updateFunction =
-                  dataConfigurations[
-                    'motor_data_configuration_starting_action'
-                  ] === 'increment'
-                    ? (val1: number, val2: number) => val1 - val2
-                    : (val1: number, val2: number) => val1 + val2;
-              }
+                  'motor_data_configuration_starting_action'
+                ] === 'increment'
+                  ? (val1: number, val2: number) => val1 - val2
+                  : (val1: number, val2: number) => val1 + val2;
             }
-            //getting last row
-            const lastMotorData = await MotorData.findOne({
-              where: {
-                motor_type_id: parseInt(motorId),
-              },
-              order: [['motor_data_created_at', 'DESC']],
-            });
-
-            await MotorData.create({
-              motor_type_id: parseInt(motorId),
-              motor_data_bearing_condition: updateFunction(
-                dataCount === 0 //if no data generated use motorType mean values
-                  ? dataConfigurations['motorType.motor_type_bearing_condition']
-                  : lastMotorData.motor_data_bearing_condition,
-                dataConfigurations.motor_data_configuration_bearing_condition_unit,
-              ),
-              motor_data_frequency: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_frequency']
-                  : lastMotorData.motor_data_frequency,
-                dataConfigurations.motor_data_configuration_frequency_unit,
-              ),
-              motor_data_input_voltage: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_input_voltage']
-                  : lastMotorData.motor_data_input_voltage,
-                dataConfigurations.motor_data_configuration_input_voltage_unit,
-              ),
-              motor_data_load: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_load']
-                  : lastMotorData.motor_data_load,
-                dataConfigurations.motor_data_configuration_load_unit,
-              ),
-              motor_data_rated_current: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_rated_current']
-                  : lastMotorData.motor_data_rated_current,
-                dataConfigurations.motor_data_configuration_rated_current_unit,
-              ),
-              motor_data_rpm: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_rpm']
-                  : lastMotorData.motor_data_rpm,
-                dataConfigurations.motor_data_configuration_rpm_unit,
-              ),
-              motor_data_starting_current: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_starting_current']
-                  : lastMotorData.motor_data_starting_current,
-                dataConfigurations.motor_data_configuration_starting_current_unit,
-              ),
-              motor_data_temperature: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_temperature']
-                  : lastMotorData.motor_data_temperature,
-                dataConfigurations.motor_data_configuration_temperature_unit,
-              ),
-              motor_data_vibration: updateFunction(
-                dataCount === 0
-                  ? dataConfigurations['motorType.motor_type_vibration']
-                  : lastMotorData.motor_data_vibration,
-                dataConfigurations.motor_data_configuration_vibration_unit,
-              ),
-            });
-            console.log(
-              `running cronjob : ${
-                dataConfigurations['motorType.motor_type_name']
-              }  @ ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
-            );
-          } catch (error) {
-            console.log('motor data generation error :', error);
           }
-        };
 
-        this.cronService.addCronJob(
-          dataConfigurations['motorType.motor_type_name'],
-          interval,
-          updateData,
-        );
-      } else {
-        //update data configuration
-        await MotorDataConfiguration.update(
-          { motor_data_generation_started: false },
-          {
+          //getting last row
+          const lastMotorData = await MotorData.findOne({
             where: {
-              motor_type_id: motorId,
+              motor_type_id: parseInt(motorId),
             },
-          },
-        );
-        //stop cron job
-        this.cronService.deleteCronJob(
-          dataConfigurations['motorType.motor_type_name'],
-        );
-      }
-      return responseModel(`data generation ${action}ed`);
+            order: [['motor_data_created_at', 'DESC']],
+          });
+
+          //add data
+          await MotorData.create({
+            motor_type_id: parseInt(motorId),
+            motor_data_bearing_condition: updateFunction(
+              dataCount === 0 //if no data generated use motorType mean values
+                ? dataConfigurations['motorType.motor_type_bearing_condition']
+                : lastMotorData.motor_data_bearing_condition,
+              dataConfigurations.motor_data_configuration_bearing_condition_unit,
+            ),
+            motor_data_frequency: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_frequency']
+                : lastMotorData.motor_data_frequency,
+              dataConfigurations.motor_data_configuration_frequency_unit,
+            ),
+            motor_data_input_voltage: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_input_voltage']
+                : lastMotorData.motor_data_input_voltage,
+              dataConfigurations.motor_data_configuration_input_voltage_unit,
+            ),
+            motor_data_load: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_load']
+                : lastMotorData.motor_data_load,
+              dataConfigurations.motor_data_configuration_load_unit,
+            ),
+            motor_data_rated_current: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_rated_current']
+                : lastMotorData.motor_data_rated_current,
+              dataConfigurations.motor_data_configuration_rated_current_unit,
+            ),
+            motor_data_rpm: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_rpm']
+                : lastMotorData.motor_data_rpm,
+              dataConfigurations.motor_data_configuration_rpm_unit,
+            ),
+            motor_data_starting_current: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_starting_current']
+                : lastMotorData.motor_data_starting_current,
+              dataConfigurations.motor_data_configuration_starting_current_unit,
+            ),
+            motor_data_temperature: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_temperature']
+                : lastMotorData.motor_data_temperature,
+              dataConfigurations.motor_data_configuration_temperature_unit,
+            ),
+            motor_data_vibration: updateFunction(
+              dataCount === 0
+                ? dataConfigurations['motorType.motor_type_vibration']
+                : lastMotorData.motor_data_vibration,
+              dataConfigurations.motor_data_configuration_vibration_unit,
+            ),
+          });
+          console.log(
+            `running cronjob : ${
+              dataConfigurations['motorType.motor_type_name']
+            }  @ ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+          );
+        } else {
+          await updateDataConfiguration(false);
+          //stop cron job
+          this.cronService.deleteCronJob(
+            dataConfigurations['motorType.motor_type_name'],
+          );
+        }
+      };
     } catch (error) {
-      console.log(error);
+      if (error instanceof HttpException) throw error;
 
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-}
-
-//2023-03-28 12:21:55
-function formatDate() {
-  const today = new Date();
-  const yyyy = today.getFullYear();
-  let mm: any = today.getMonth() + 1; // Months start at 0!
-  let dd: any = today.getDate();
-  const hours = today.getHours();
-  const minutes = today.getMinutes();
-  const seconds = today.getSeconds();
-
-  if (dd < 10) dd = '0' + dd;
-  if (mm < 10) mm = '0' + mm;
-
-  return `${dd}-${mm}-${yyyy} ${hours}:${minutes}:${seconds}`;
 }
